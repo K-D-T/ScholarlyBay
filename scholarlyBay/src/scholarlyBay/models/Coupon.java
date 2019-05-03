2bf44776be9e20bdc835ab8bce9fb23d9a1df33b
kdthomas <kdthomas@iastate.edu>
2019-05-03 18:30:39 -0500

    Add new file

---
+++ scholarlyBay/src/scholarlyBay/models/Coupon.java
@@ -0,0 +1,15 @@
+package scholarlyBay.models;
+
+public class Coupon {
+	public int code;
+	public double percentOff;
+	
+	public Coupon(int code, double percentOff) {
+		this.code = code;
+		this.percentOff = percentOff;
+	}
+
+	
+
+	
+}
